9909c05d90f10af8d9c3d5671cc9e37041135814
Will Carter <will.carter@fergusllc.com>
2019-05-10 09:28:14 -0600

    changed port

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 4000,
+  port = process.env.PORT || 8670,
   bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(express.static(__dirname + "/public"));
   
 var routes = require('./api/routes/h3Routes'); //importing route
 routes(app); //register the route
