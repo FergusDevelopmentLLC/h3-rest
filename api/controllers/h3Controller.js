c9a01bd78835e36692cd3dea7ce411243bcbbed2
Will Carter <will.carter@fergusllc.com>
2019-09-08 15:07:46 -0600

    removed level 7 hex zoom

--- api/controllers/h3Controller.js
+++ api/controllers/h3Controller.js
@@ -12,7 +12,7 @@ const h3_03_data = require('./data/hex3_03_mcdonalds.json')
 const h3_04_data = require('./data/hex3_04_mcdonalds.json')
 const h3_05_data = require('./data/hex3_05_mcdonalds.json')
 const h3_06_data = require('./data/hex3_06_mcdonalds.json')
-const h3_07_data = require('./data/hex3_07_mcdonalds.json')
+//const h3_07_data = require('./data/hex3_07_mcdonalds.json')
 
 exports.getH3BinsForExtent = (req, res) => {
 
@@ -193,7 +193,7 @@ joinFeatureToData = (featureCollection, res) => {
   else if (res == 4) setCountsForFeatureCollection(featureCollection, h3_04_data)
   else if (res == 5) setCountsForFeatureCollection(featureCollection, h3_05_data)
   else if (res == 6) setCountsForFeatureCollection(featureCollection, h3_06_data)
-  else if (res == 7) setCountsForFeatureCollection(featureCollection, h3_07_data)
+  //else if (res == 7) setCountsForFeatureCollection(featureCollection, h3_07_data)
 
   return featureCollection
 }
